e48b0a163280b3c8c42f2b5ffa6f90dc3bd2660a
samaorahno <samabos007@gmail.com>
2019-01-22 12:08:06 +0100

    ch-Add-views-to-ghpages

--- UI/js/script.js
+++ UI/js/script.js
@@ -12,39 +12,3 @@ function togle_nav_func() {
     elem.style.display = 'none';
   }
 }
-
-
-/*function togle_nav_func(){
-	let elem = document.getElementById("togle_nav");
-
-	
-		if (elem.currentStyle) {
-			displayStyle = elem.currentStyle.display;
-		} else if (window.getComputedStyle) {
-			displayStyle = window.getComputedStyle(elem, null).getPropertyValue("display");
-		}
-		if(displayStyle === "none")
-		{
-			elem.style.display = "block";
-		}
-		else{
-			elem.style.display = "none";
-		}
-	
-}
-
-
-function togle_nav_func() {
-  let elem = document.getElementById("togle_nav");
-  //let displayStyle = 'none';
-  if (elem.currentStyle) {
-    var displayStyle = elem.currentStyle.display;
-  } else if (window.getComputedStyle) {
-    var displayStyle = window.getComputedStyle(elem, null).getPropertyValue("display");
-  }
-  if (displayStyle === "none") {
-    elem.style.display = "block";
-  } else {
-    elem.style.display = "none";
-  }
-*/
